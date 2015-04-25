50f4e4304d6d5e54ffbf78189e5c5cc74dc28da2
Michael Taufen <mtaufen@gmail.com>
2015-04-25 05:28:45 -0500

    Analysis Set List displays things more or less correctly, save for broken progres bars

--- src/cljs/hybsearch/rpc.cljs
+++ src/cljs/hybsearch/rpc.cljs
@@ -3,7 +3,139 @@
     [tailrecursion.javelin :refer [defc defc=]])
   (:require
    [tailrecursion.javelin]
-   [tailrecursion.castra :refer [mkremote]]))
+   [tailrecursion.castra :refer [mkremote]]
+   [datascript :as d]))
+
+(def schema {:analysis-set/name           {:db/cardinality :db.cardinality/one}
+             :analysis-set/loci           {:db/cardinality :db.cardinality/many}
+             :analysis-set/clustal-scheme {:db/cardinality :db.cardinality/one  :db/valueType :db.type/ref}
+             :analysis-set/jobs           {:db/cardinality :db.cardinality/many :db/valueType :db.type/ref}
+             :analysis-set/num-triples    {:db/cardinality :db.cardinality/one}
+             :analysis-set/num-processed-triples {:db/cardinality :db.cardinality/one }
+
+             :job/name                    {:db/cardinality :db.cardinality/one }
+             :job/loci                    {:db/cardinality :db.cardinality/many}
+             :job/num-triples             {:db/cardinality :db.cardinality/one }
+             :job/num-processed-triples   {:db/cardinality :db.cardinality/one }
+             :job/analysis-set            {:db/cardinality :db.cardinality/one :db/valueType :db.type/ref}
+
+             :clustal-scheme/ex-setting   {:db/cardinality :db.cardinality/one}
+
+             :locus/accession-num         {:db/cardinality :db.cardinality/one :db/unique :db.unique/identity}
+             :locus/binomial              {:db/cardinality :db.cardinality/one}
+
+             ;; Todo: Eventually allow more locus information on client.
+             ;; There is also more species information than the binomial available in the GenBank files, i.e. the ncbi_taxid
+             ;; Will probably also need to enforce uniqueness on the clustal-schemes
+             })
+
+(defonce conn (d/create-conn schema))
+
+(def seed-data [
+                {:db/id -20
+                 :locus/accession-num "HM233091"
+                 :locus/binomial      "Lepus mandshuricus"}
+                {:db/id -21
+                 :locus/accession-num "AB687524"
+                 :locus/binomial      "Lepus timidus"}
+                {:db/id -22
+                 :locus/accession-num "AB687525"
+                 :locus/binomial      "Lepus timidus"}
+
+                {:db/id -1
+                 :clustal-scheme/ex-setting "Example Option 1"} ;; Todo: Maybe even allow people to view and manage their clustal schemes separately from analysis sets
+
+                {:db/id -2
+                 :job/name "Lepus 1"
+                 :job/loci ["HM233091", "AB687524", "AB687525"]
+                 :job/analysis-set -4
+                 :job/num-triples 1
+                 :job/num-processed-triples 1}
+
+                {:db/id -3
+                 :job/name "Lepus 2"
+                 :job/loci ["HM233091", "AB687524", "AB687525"]
+                 :job/analysis-set -4
+                 :job/num-triples 1
+                 :job/num-processed-triples 1}
+
+                {:db/id -4
+                 :analysis-set/name "Set 1"
+                 :analysis-set/jobs [-2, -3]
+                 :analysis-set/clustal-scheme -1
+                 :analysis-set/loci ["HM233091", "AB687524", "AB687525"]
+                 :analysis-set/num-triples 1
+                 :analysis-set/num-processed-triples 1}
+
+                {:db/id -5
+                 :analysis-set/name "Empty Set"
+                 :analysis-set/jobs []
+                 :analysis-set/clustal-scheme -1
+                 :analysis-set/loci []
+                 :analysis-set/num-triples 0
+                 :analysis-set/num-processed-triples 0}
+
+                {:db/id -6
+                 :analysis-set/name "Empty Set"
+                 :analysis-set/jobs []
+                 :analysis-set/clustal-scheme -1
+                 :analysis-set/loci []
+                 :analysis-set/num-triples 0
+                 :analysis-set/num-processed-triples 0}
+
+                {:db/id -7
+                 :analysis-set/name "Empty Set"
+                 :analysis-set/jobs []
+                 :analysis-set/clustal-scheme -1
+                 :analysis-set/loci []
+                 :analysis-set/num-triples 0
+                 :analysis-set/num-processed-triples 0}
+
+                {:db/id -8
+                 :analysis-set/name "Empty Set"
+                 :analysis-set/jobs []
+                 :analysis-set/clustal-scheme -1
+                 :analysis-set/loci []
+                 :analysis-set/num-triples 0
+                 :analysis-set/num-processed-triples 0}
+
+                {:db/id -9
+                 :analysis-set/name "Empty Set"
+                 :analysis-set/jobs []
+                 :analysis-set/clustal-scheme -1
+                 :analysis-set/loci []
+                 :analysis-set/num-triples 0
+                 :analysis-set/num-processed-triples 0}
+
+                {:db/id -10
+                 :analysis-set/name "Empty Set"
+                 :analysis-set/jobs []
+                 :analysis-set/clustal-scheme -1
+                 :analysis-set/loci []
+                 :analysis-set/num-triples 0
+                 :analysis-set/num-processed-triples 0}
+
+                {:db/id -12
+                 :analysis-set/name "Empty Set"
+                 :analysis-set/jobs []
+                 :analysis-set/clustal-scheme -1
+                 :analysis-set/loci []
+                 :analysis-set/num-triples 0
+                 :analysis-set/num-processed-triples 0}
+
+                {:db/id -11
+                 :analysis-set/name "Empty Set"
+                 :analysis-set/jobs []
+                 :analysis-set/clustal-scheme -1
+                 :analysis-set/loci []
+                 :analysis-set/num-triples 0
+                 :analysis-set/num-processed-triples 0}
+
+
+                ])
+
+(d/transact! conn seed-data)
+
 
 (defc state {:random nil})
 (defc error nil)
@@ -11,10 +143,25 @@
 
 (defc= random-number (get state :random))
 
+(defc= analysis-sets (d/q '[ :find ?e ?name ?processed ?total
+                               :where [?e :analysis-set/name ?name]
+                                      [?e :analysis-set/num-triples ?total]
+                                      [?e :analysis-set/num-processed-triples ?processed]
+                              ] @conn))
+
+(defc selected-analysis-set-id (first (first @analysis-sets))) ;; Holds the id of the currently selected analysis set
+
+
+(enable-console-print!)
+(print "Query Result:")
+(print analysis-sets)
+
+
+
 (def get-state
   (mkremote 'hybsearch.api/get-state state error loading))
 
 (defn init []
-  ;;(get-state)
-  ;;(js/setInterval get-state 1000)
+  (get-state)
+  (js/setInterval get-state 1000)
   )
