f7fc5ac32e8ef1b9209f4a249e1c1078f3beccf4
Michael Taufen <mtaufen@gmail.com>
2015-04-26 18:44:15 -0500

    Updated UI to fit new data model, still having an issue with the initial queries

--- src/cljs/hybsearch/rpc.cljs
+++ src/cljs/hybsearch/rpc.cljs
@@ -2,27 +2,38 @@
   (:require-macros
     [tailrecursion.javelin :refer [defc defc=]])
   (:require
-   [tailrecursion.javelin]
+   [tailrecursion.javelin :refer [cell-map]]
    [tailrecursion.castra :refer [mkremote]]
    [datascript :as d]))
 
-(def schema {:analysis-set/name           {:db/cardinality :db.cardinality/one}
-             :analysis-set/loci           {:db/cardinality :db.cardinality/many}
-             :analysis-set/clustal-scheme {:db/cardinality :db.cardinality/one  :db/valueType :db.type/ref}
-             :analysis-set/jobs           {:db/cardinality :db.cardinality/many :db/valueType :db.type/ref}
-             :analysis-set/num-triples    {:db/cardinality :db.cardinality/one}
-             :analysis-set/num-processed-triples {:db/cardinality :db.cardinality/one }
+(enable-console-print!)
 
-             :job/name                    {:db/cardinality :db.cardinality/one }
-             :job/loci                    {:db/cardinality :db.cardinality/many}
-             :job/num-triples             {:db/cardinality :db.cardinality/one }
-             :job/num-processed-triples   {:db/cardinality :db.cardinality/one }
-             :job/analysis-set            {:db/cardinality :db.cardinality/one :db/valueType :db.type/ref}
+(def schema {:clustal-scheme/name                  {:db/cardinality :db.cardinality/one}
+             :clustal-scheme/ex-setting            {:db/cardinality :db.cardinality/one}
+             :clustal-scheme/num-triples           {:db/cardinality :db.cardinality/one} ;; Always the same, equal to the number of triples that can be created for all loci
+             :clustal-scheme/num-processed-triples {:db/cardinality :db.cardinality/one} ;; Depends on how well processed the global set is for this clustal scheme
 
-             :clustal-scheme/ex-setting   {:db/cardinality :db.cardinality/one}
+             :analysis-set/name                    {:db/cardinality :db.cardinality/one}
+             :analysis-set/set-def                 {:db/cardinality :db.cardinality/one :db/valueType :db.type/ref}
+             :analysis-set/num-triples             {:db/cardinality :db.cardinality/one}
+             :analysis-set/num-processed-triples   {:db/cardinality :db.cardinality/one}
 
-             :locus/accession-num         {:db/cardinality :db.cardinality/one :db/unique :db.unique/identity}
-             :locus/binomial              {:db/cardinality :db.cardinality/one}
+             :job/name                             {:db/cardinality :db.cardinality/one}
+             :job/set-def                          {:db/cardinality :db.cardinality/one :db/valueType :db.type/ref}
+             :job/num-triples                      {:db/cardinality :db.cardinality/one}
+             :job/num-processed-triples            {:db/cardinality :db.cardinality/one}
+             :job/clustal-scheme                   {:db/cardinality :db.cardinality/one :db/valueType :db.type/ref}
+
+
+             ;; Todo: how to match up relational ref ids when downloading data from server?
+
+             ;; all triples for the set = triples(loci for each binomial UNION loci list) INTERSECT analysis-set triples
+             :set-def/binomials                    {:db/cardinality :db.cardinality/many} ;; Currently a list of binomial species names
+             :set-def/loci                         {:db/cardinality :db.cardinality/many} ;; Currently a list of accession numbers
+             :set-def/analysis-set                 {:db/cardinality :db.cardinality/one :db/valueType :db.type/ref} ;; Optional, leave empty to indicate global analysis set, or if this is the set-def for an analysis set
+
+             :locus/accession-num                  {:db/cardinality :db.cardinality/one :db/unique :db.unique/identity}
+             :locus/binomial                       {:db/cardinality :db.cardinality/one}
 
              ;; Todo: Eventually allow more locus information on client.
              ;; There is also more species information than the binomial available in the GenBank files, i.e. the ncbi_taxid
@@ -43,22 +54,38 @@
                  :locus/binomial      "Lepus timidus"}
 
                 {:db/id -1
-                 :clustal-scheme/ex-setting "Example Option 1"} ;; Todo: Maybe even allow people to view and manage their clustal schemes separately from analysis sets
+                 :clustal-scheme/name       "Scheme 1"
+                 :clustal-scheme/ex-setting "Example Option 1"
+                 :clustal-scheme/num-triples 900
+                 :clustal-scheme/num-processed-triples 23
+                 } ;; Todo: Maybe even allow people to view and manage their clustal schemes separately from analysis sets
 
                 {:db/id -80
-                 :clustal-scheme/ex-setting "Example Option 2"}
+                 :clustal-scheme/name       "Scheme 2"
+                 :clustal-scheme/ex-setting "Example Option 2"
+                 :clustal-scheme/num-triples 900
+                 :clustal-scheme/num-processed-triples 67
+                 }
+
+
+                {:db/id -90
+                 :set-def/loci ["HM233091", "AB687524", "AB687525"]}
+
+                {:db/id -100
+                 :set-def/loci []}
+
 
                 {:db/id -2
                  :job/name "Lepus 1"
-                 :job/loci ["HM233091", "AB687524", "AB687525"]
-                 :job/analysis-set -4
+                 :job/clustal-scheme -80
+                 :job/set-def -90
                  :job/num-triples 1
                  :job/num-processed-triples 1}
 
                 {:db/id -3
                  :job/name "Lepus 2"
-                 :job/loci ["HM233091", "AB687524", "AB687525"]
-                 :job/analysis-set -4
+                 :job/set-def -90
+                 :job/clustal-scheme -80
                  :job/num-triples 1
                  :job/num-processed-triples 1}
 
@@ -66,7 +93,7 @@
                  :analysis-set/name "A Set 1"
                  :analysis-set/jobs [-2, -3]
                  :analysis-set/clustal-scheme -80
-                 :analysis-set/loci ["HM233091", "AB687524", "AB687525"]
+                 :analysis-set/set-def -90
                  :analysis-set/num-triples 1
                  :analysis-set/num-processed-triples 1}
 
@@ -74,14 +101,9 @@
                  :analysis-set/name "Empty Set"
                  :analysis-set/jobs []
                  :analysis-set/clustal-scheme -1
-                 :analysis-set/loci []
+                 :analysis-set/set-def -100
                  :analysis-set/num-triples 0
                  :analysis-set/num-processed-triples 0}
-
-
-
-
-
 
                 ])
 
@@ -92,17 +114,26 @@
 (defc error nil)
 (defc loading [])
 
-(defc= analysis-sets (d/q '[ :find ?e ?name ?processed ?total
-                               :where [?e :analysis-set/name ?name]
-                                      [?e :analysis-set/num-triples ?total]
-                                      [?e :analysis-set/num-processed-triples ?processed]
-                              ] @db))
+(print "hi 1")
 
-(defc selected-analysis-set-id (first (first @analysis-sets))) ;; Holds the id of the currently selected analysis set
-(defc= selected-analysis-set (d/entity @db selected-analysis-set-id))
+;; Todo: wish there was a better way to query than just by name
+;; Clustal Schemes
+;;(defc= clustal-scheme-ids (d/q '[find ?e :where [?e :clustal-scheme/name ?name]] @db))
+;; (defc= clustal-schemes (cell-map (fn [e] (d/entity @db e)) clustal-scheme-ids))
+
+;; (defc selected-clustal-scheme-id (first (first @clustal-scheme-ids))) ;; Holds the id of the currently selected analysis set
+;; (defc= selected-clustal-scheme (d/entity @db selected-clustal-scheme-id))
+
+;; ;; Analysis Sets
+;; (defc= analysis-set-ids (d/q '[find ?e :where [?e :analysis-set/name ?name]] @db))
+;; (defc= analysis-sets (cell-map (fn [e] (d/entity @db e)) analysis-set-ids))
+
+;; (defc selected-analysis-set-id (first (first @analysis-set-ids))) ;; Holds the id of the currently selected analysis set
+;; (defc= selected-analysis-set (d/entity @db selected-analysis-set-id))
 
 
-(enable-console-print!)
+
+(print "hi 2")
 
 
 
