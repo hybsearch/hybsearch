19e4ba04750c15fba5773c593e8c418beb1c9998
Hawken Rives <hawkrives@gmail.com>
2015-10-29 16:12:57 -0500

    something

--- js/bannana.js
+++ js/bannana.js
@@ -4,12 +4,16 @@ const convert = require('./genbank-to-fasta').convertFiles
 const clustal = require('./run-clustal').clustal
 const fs = require('fs')
 
+function bannana(file) {
+	const output = convert(file)
+	clustal(fs.readFileSync(output), {tree: true, output: 'FASTA', infile: output})
+}
+
 function main() {
 	if (process.argv.length != 3) {
 		process.exit(1)
 	}
-	const output = convert(process.argv[2])
-	clustal(fs.readFileSync(output), {tree: true, output: 'FASTA', infile: output})
+	bannana(process.argv[2])
 }
 
 if (require.main === module) {
