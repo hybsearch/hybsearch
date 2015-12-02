805fddca83fa00e7bc0ae46429e148611d199a36
Hawken Rives <hawkrives@gmail.com>
2015-12-02 13:25:52 -0600

    stuff

--- electron/run-ninja.js
+++ electron/run-ninja.js
@@ -4,4 +4,4 @@
 const fs = require('fs')
 const ninja = require('./ninja.js')
 
-ninja(fs.readFileSync(process.argv[2]), '.ninjaout')
+fs.writeFileSync(process.argv[3], ninja(fs.readFileSync(process.argv[2]), '.ninjaout'), 'utf-8')
