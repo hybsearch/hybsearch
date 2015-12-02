11ce62e69420d69261b88ffb59a1b5b63e2b6adc
Hawken Rives <hawkrives@gmail.com>
2015-12-02 13:40:33 -0600

    fix clustal running

--- electron/run-clustal.js
+++ electron/run-clustal.js
@@ -2,6 +2,18 @@
 'use strict'
 
 const fs = require('fs')
-const ninja = require('./ninja.js')
+const clustal = require('./clustal.js')
 
-ninja(fs.readFileSync(process.argv[2]), '.ninjaout')
+const args = {
+	align: true,
+	pwgapopen: 15,
+	pwgapext: 6.66,
+	pwdnamatrix: 'IUB',
+	transweight: 0.5,
+	gapext: 6.66,
+	gapopen: 15,
+	numiter: 1,
+	output: 'FASTA',
+}
+
+clustal(fs.readFileSync(process.argv[2], 'utf-8'), args, '.aln')
