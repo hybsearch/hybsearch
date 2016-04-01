80cbb12466372f24cd5e64f2926c678ac5bcc50a
Hawken Rives <hawkrives@gmail.com>
2016-03-31 21:21:10 -0500

    add consensus-tree-to-newick converter

---
+++ electron/bin/consensus-newick.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+'use strict'
+
+const getData = require('./lib_get-data')
+
+module.exports = consensusTreeNewick
+function consensusTreeNewick(input) {
+	let labelRegex = /taxlabels(\n\s*[^;]*)+\s;/
+	let labels = labelRegex.exec(input)[1]
+	labels = labels.split('\n').map(l => l.trim()).filter(x => x)
+
+	let treeRegex = /tree\s+\S+\s+=\s+\[[^\n\r\]]+\]\s+([^;\n\r]+;)/g
+	let conTree = treeRegex.exec(input)[1]
+	conTree = conTree.replace(/\[[^\]]+\]/g, "")
+
+	for (let i = 0; i < labels.length; i++) {
+		// replace numbers with names
+		conTree = conTree.replace(`(${i + 1}:`, `(${labels[i]}:`)
+		conTree = conTree.replace(`,${i + 1}:`, `,${labels[i]}:`)
+	}
+
+	return conTree
+}
+
+function main() {
+	let file = process.argv[2]
+
+	if (!file && file !== '-') {
+		console.error('usage: node consensus-newick.js (<input> | -)')
+		process.exit(1)
+	}
+
+	getData(file)
+		.then(consensusTreeNewick)
+		.then(data => console.log(data))
+}
+
+if (require.main === module) {
+	main()
+}
