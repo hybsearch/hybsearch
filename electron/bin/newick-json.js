79eaef7b182ea8471887e9d848649dcc53fc5b8d
Hawken Rives <hawkrives@gmail.com>
2016-04-01 14:56:30 -0500

    add a script to convert newick to a json file

---
+++ electron/bin/newick-json.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+'use strict'
+
+const getData = require('./lib_get-data')
+const newick = require('../vendor/newick').parse
+
+function main() {
+	let file = process.argv[2]
+
+	if (!file && file !== '-') {
+		console.error('usage: node fasta-to-nexus.js (<input> | -)')
+		process.exit(1)
+	}
+
+	getData(file)
+		.then(newick)
+		.then(d => JSON.stringify(d))
+		.then(data => console.log(data))
+		.catch(console.error.bind(console))
+}
+
+if (require.main === module) {
+	main()
+}
