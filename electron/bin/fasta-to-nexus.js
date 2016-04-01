a387146e3a4b38b9a0dce96d05b2d93330c45982
Hawken Rives <hawkrives@gmail.com>
2016-03-31 22:13:01 -0500

    switch to seqmagick for translating between fasta and nexus

--- electron/bin/fasta-to-nexus.js
+++ electron/bin/fasta-to-nexus.js
@@ -21,7 +21,6 @@ function makeClustalArguments(args) {
 	return argString
 }
 
-module.exports = clustal
 function clustal(data) {
 	const inputFile = tempfile().replace(' ', '\ ')
 	const outputFile = tempfile().replace(' ', '\ ')
@@ -41,16 +40,30 @@ function clustal(data) {
 	return fs.readFileSync(outputFile, 'utf-8')
 }
 
+module.exports = seqmagick
+function seqmagick(data) {
+	const inputFile = tempfile().replace(' ', '\ ')
+	const outputFile = tempfile().replace(' ', '\ ')
+	fs.writeFileSync(inputFile, data, 'utf-8')
+
+	child.execSync(`seqmagick convert --input-format fasta --output-format nexus --alphabet dna ${inputFile} ${outputFile}`)
+
+	// seqmagick is wrapping the identifiers in quotes
+	// mrbayes does not like single quotes
+	// remove them
+	return fs.readFileSync(outputFile, 'utf-8').replace(/'/g, "")
+}
+
 function main() {
 	let file = process.argv[2]
 
 	if (!file && file !== '-') {
-		console.error('usage: node clustal.js (<input> | -)')
+		console.error('usage: node fasta-to-nexus.js (<input> | -)')
 		process.exit(1)
 	}
 
 	getData(file)
-		.then(clustal)
+		.then(seqmagick)
 		.then(data => console.log(data))
 		.catch(console.error.bind(console))
 }
