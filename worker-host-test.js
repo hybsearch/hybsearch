2629ff9ed3a508a7573bfdac7bc71bfe7ce742bb
Hawken Rives <hawkrives@gmail.com>
2016-04-07 21:20:42 -0400

    clean up the worker test commander

--- worker-host-test.js
+++ worker-host-test.js
@@ -2,23 +2,13 @@
 
 const childProcess = require('child_process')
 
-// let file = {path: 'data/emydura-short.gb'}
-// let cmd = 'node'
-// let args = ['./worker.js', file.path]
-// let opts = {
-// 	encoding: 'utf8',
-// 	maxBuffer: 400 * 1024, // 400KB
-// 	stdio: ['ignore', 'ipc', 'pipe'],
-// }
-
-// let child = childProcess.spawn(cmd, opts)
-
 let child = childProcess.fork(__dirname + '/worker.js')
 
 child.on('message', communique => {
 	let cmd = communique[0]
 	let msg = communique[1]
-	if (cmd == 'error') {
+
+	if (cmd === 'error') {
 		console.error('error!', msg)
 	}
 	else {
@@ -31,4 +21,3 @@ child.on('message', communique => {
 })
 
 child.send('data/ent.gb')
-// child.send('data/emydura-short.gb')
