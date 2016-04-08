c529175222ced5067c6d894e3aaf2023d88a8ef4
Hawken Rives <hawkrives@gmail.com>
2016-04-07 23:32:48 -0400

    rework ent to be able to run on non-binary trees

--- ent.js
+++ ent.js
@@ -3,6 +3,8 @@
 
 const getData = require('./bin/lib_get-data')
 require('./vendor/array.proto.includes')
+const combs = require('combinations-generator')
+const uniqBy = require('lodash/uniqBy')
 
 const ENABLE_DEBUG = false
 let debug = ENABLE_DEBUG && console.log.bind(console) || () => {}
@@ -10,14 +12,10 @@ let debug = ENABLE_DEBUG && console.log.bind(console) || () => {}
 let pluck = (arr, key) => arr.map(x => x[key])
 let label = node => `${node.name}-${node.ident}`
 
-function recordnm(species1, species2) {
-	console.log(`nonmonophyly: ${label(species1)} / ${label(species2)}`)
-}
-
-function marknm(node, species1, species2) {
+function nmMark(node, species1, species2) {
 	if (node.branchset) {
-		marknm(node.branchset[0], species1, species2)
-		marknm(node.branchset[1], species1, species2)
+		nmMark(node.branchset[0], species1, species2)
+		nmMark(node.branchset[1], species1, species2)
 	}
 	else if (node.name === species1.name) {
 		node.nm_inner = node.nm_inner || []
@@ -29,43 +27,58 @@ function marknm(node, species1, species2) {
 	}
 }
 
-function mutatenm(node) {
+function nmSearch(node) {
 	let name = node.name
-	node.ident = name.split('-')[1]
-	node.name = name.split('-')[0]
+	if (name && !node.ident) {
+		// console.log(name)
+		node.ident = name.split('-')[1]
+		node.name = name.split('-')[0]
+	}
 
 	if (node.branchset) {
 		debug('has branchset')
-		let speciesListA = mutatenm(node.branchset[0])
-		let speciesListB = mutatenm(node.branchset[1])
+		let combinations = combs(node.branchset, 2)
 
-		debug('speciesListA:', speciesListA, 'speciesListB', speciesListB)
-		speciesListA.forEach(species1 => {
-			let hasName = pluck(speciesListB, 'name').includes(species1.name)
-			let allEqual = !(pluck(speciesListB, 'name').every(n => n === species1))
-			debug(`included: ${hasName}; all equal: ${allEqual}`)
+		let speciesList = []
+		for (let speciesSet of combinations) {
+			let speciesListA = nmSearch(speciesSet[0])
+			let speciesListB = nmSearch(speciesSet[1])
 
-			if (hasName && allEqual) {
-				speciesListB.forEach(species2 => {
-					if (species2.name !== species1.name) {
-						marknm(node, species1, species2)
-						recordnm(species1, species2)
-						debug(`marknm called on ${species1} and ${species2}`)
-					}
-				})
+			debug('speciesListA:', speciesListA, 'speciesListB', speciesListB)
 
-				speciesListA.forEach(species3 => {
-					if (species3.name !== species1.name) {
-						marknm(node, species1, species3)
-						recordnm(species1, species3)
-						debug(`marknm called on ${species1} and ${species3}`)
-					}
-				})
-			}
-		})
-		let speciesList = [].concat(speciesListA, speciesListB)
+			let speciesBNames = pluck(speciesListB, 'name')
+			speciesListA.forEach(species1 => {
+				let hasName = speciesBNames.includes(species1.name)
+				let notAllEqual = !(speciesBNames.every(n => n === species1))
+				debug(`included: ${hasName}; not all equal: ${notAllEqual}`)
+
+				// species1 is in speciesListB, and not everything in speciesListB is species1
+				if (hasName && notAllEqual) {
+					// search in speciesListB
+					speciesListB.forEach(species2 => {
+						if (species2.name !== species1.name) {
+							nmMark(node, species1, species2)
+							console.log(`nonmonophyly: ${label(species1)} / ${label(species2)}`)
+							debug(`nmMark called on ${species1} and ${species2}`)
+						}
+					})
+
+					// and search in speciesListA
+					speciesListA.forEach(species3 => {
+						if (species3.name !== species1.name) {
+							nmMark(node, species1, species3)
+							console.log(`nonmonophyly: ${label(species1)} / ${label(species3)}`)
+							debug(`nmMark called on ${species1} and ${species3}`)
+						}
+					})
+				}
+			})
+
+			speciesList = speciesList.concat(speciesListA, speciesListB)
+		}
+
+		speciesList = uniqBy(speciesList, 'ident')
 		debug('speciesList', speciesList)
-
 		return speciesList
 	}
 
@@ -85,7 +98,7 @@ function main() {
 
 	getData(file)
 		.then(d => JSON.parse(d))
-		.then(mutatenm)
+		.then(nmSearch)
 		.catch(console.error.bind(console))
 }
 
