73a2000109e6bd33aedd88fbdfb127442a8d88ff
Hawken Rives <hawkrives@gmail.com>
2016-04-07 21:10:38 -0400

    clean up bits of ent.js

--- ent.js
+++ ent.js
@@ -2,10 +2,14 @@
 'use strict'
 
 const getData = require('./bin/lib_get-data')
-const _ = require('lodash')
+require('./vendor/array.proto.includes')
+
+const ENABLE_DEBUG = false
+const equals = rhs => lhs => lhs === rhs
+const debug = ENABLE_DEBUG && console.log.bind(console) || () => {}
 
 function recordnm(species1, species2) {
-	console.log(`Nonmonophyly found: ${species1} and ${species2}`)
+	console.log(`nonmonophyly: ${species1} / ${species2}`)
 }
 
 function marknm(node, species1, species2) {
@@ -13,70 +17,64 @@ function marknm(node, species1, species2) {
 		marknm(node.branchset[0], species1, species2)
 		marknm(node.branchset[1], species1, species2)
 	} else if (node.name === species1) {
-		node.nminner = node.nminner || []
-		node.nminner.push(species2)
+		node.nm_inner = node.nm_inner || []
+		node.nm_inner.push(species2)
 	} else if (node.name === species2) {
-		node.nmouter = node.nmouter || []
-		node.nmouter.push(species1)
+		node.nm_outer = node.nm_outer || []
+		node.nm_outer.push(species1)
 	}
 }
 
+function getName(name) {
+	return name.split('-')[0]
+}
+
 function mutatenm(node) {
-	// console.log(node)
+	node.ident = node.name.split('-')[1]
+	node.name = node.name.split('-')[0]
+
 	if (node.branchset) {
-		// console.log("has branchset")
-		let speciesA = mutatenm(node.branchset[0])
-		let speciesB = mutatenm(node.branchset[1])
+		debug("has branchset")
+		let speciesA = mutatenm(node.branchset[0]).map(getName)
+		let speciesB = mutatenm(node.branchset[1]).map(getName)
+
+		debug('speciesA:', speciesA, 'speciesB', speciesB)
 		speciesA.forEach(species1 => {
-			// console.log(speciesB.indexOf(species1), " ", speciesB.every(x => x === species1))
-			if ((speciesB.indexOf(species1) > -1) && (speciesB.every(x => x === species1))) {
+			debug(`included: ${speciesB.includes(species1)}; all equal: ${speciesB.every(equals(species1))}`)
+			if (speciesB.includes(species1) && !speciesB.every(equals(species1))) {
 				speciesB.forEach(species2 => {
 					if (species2 !== species1) {
 						marknm(node, species1, species2)
 						recordnm(species1, species2)
-						// console.log("markednm called on ", species1, " and ", species2)
+						debug("marknm called on ", species1, " and ", species2)
 					}
 				})
+
 				speciesA.forEach(species3 => {
 					if (species3 !== species1) {
 						marknm(node, species1, species3)
 						recordnm(species1, species3)
-						// console.log("markednm called on ", species1, " and ", species3)
+						debug("marknm called on ", species1, " and ", species3)
 					}
 				})
 			}
 		})
-		let speciesList = []
-		speciesList = speciesList.concat(speciesA, speciesB)
-		// console.log(speciesList)
+		let speciesList = [].concat(speciesA, speciesB)
+		debug('speciesList', speciesList)
 
 		return speciesList
 	}
 
-	// console.log("no branchset, name: ", node.name)
+	debug(`no branchset, name: ${node.name}, ident: ${node.ident}`)
 	return [node.name]
 }
 
-// function main() {
-// 	if (process.argv.length < 3) {
-// 		console.error('usage: node ent.js <input>')
-// 		process.exit(1)
-// 	}
-
-// 	let data = JSON.parse(require('fs').readFileSync(process.argv[2], 'utf-8'))
-
-// 	let ntree = _.cloneDeep(data)
-
-// 	mutatenm(ntree)
-
-// 	// console.log(JSON.stringify(ntree, null, 2))
-// }
 
 function main() {
 	let file = process.argv[2]
 
 	if (!file && file !== '-') {
-		console.error('usage: node fasta-to-nexus.js (<input> | -)')
+		console.error('usage: node ent.js (<input> | -)')
 		process.exit(1)
 	}
 
