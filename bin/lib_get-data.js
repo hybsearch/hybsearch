0cb82eae52e7f497052064719a31d5b0a230bc76
Hawken Rives <hawkrives@gmail.com>
2016-04-07 22:28:54 -0400

    fix up getData to better accept things

--- bin/lib_get-data.js
+++ bin/lib_get-data.js
@@ -3,16 +3,16 @@ const stdin = require('get-stdin')
 const fs = require('fs')
 
 module.exports = function getData(fileOrStdin) {
-	if (fileOrStdin === '-') {
+	if (!fileOrStdin || fileOrStdin === '-') {
 		return stdin()
-	} else {
-		return new Promise((resolve, reject) => {
-			fs.readFile(fileOrStdin, 'utf-8', (err, contents) => {
-				if (err) {
-					reject(err)
-				}
-				resolve(contents)
-			})
-		})
 	}
+
+	return new Promise((resolve, reject) => {
+		fs.readFile(fileOrStdin, 'utf-8', (err, contents) => {
+			if (err) {
+				reject(err)
+			}
+			resolve(contents)
+		})
+	})
 }
