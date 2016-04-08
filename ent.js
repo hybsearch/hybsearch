9c3052bfb5c0369352e25187c8d674c925f6e4af
Hawken Rives <hawkrives@gmail.com>
2016-04-07 21:23:26 -0400

    tweak ent.js to work split up the name and identifier labels

--- ent.js
+++ ent.js
@@ -5,28 +5,26 @@ const getData = require('./bin/lib_get-data')
 require('./vendor/array.proto.includes')
 
 const ENABLE_DEBUG = false
-const equals = rhs => lhs => lhs === rhs
-const debug = ENABLE_DEBUG && console.log.bind(console) || () => {}
+let debug = ENABLE_DEBUG && console.log.bind(console) || () => {}
+
+let pluck = (arr, key) => arr.map(x => x[key])
+let label = node => `${node.name}-${node.ident}`
 
 function recordnm(species1, species2) {
-	console.log(`nonmonophyly: ${species1} / ${species2}`)
+	console.log(`nonmonophyly: ${label(species1)} / ${label(species2)}`)
 }
 
 function marknm(node, species1, species2) {
 	if (node.branchset) {
 		marknm(node.branchset[0], species1, species2)
 		marknm(node.branchset[1], species1, species2)
-	} else if (node.name === species1) {
+	} else if (node.name === species1.name) {
 		node.nm_inner = node.nm_inner || []
 		node.nm_inner.push(species2)
-	} else if (node.name === species2) {
+	} else if (node.name === species2.name) {
 		node.nm_outer = node.nm_outer || []
 		node.nm_outer.push(species1)
 	}
-}
-
-function getName(name) {
-	return name.split('-')[0]
 }
 
 function mutatenm(node) {
@@ -34,40 +32,44 @@ function mutatenm(node) {
 	node.name = node.name.split('-')[0]
 
 	if (node.branchset) {
-		debug("has branchset")
-		let speciesA = mutatenm(node.branchset[0]).map(getName)
-		let speciesB = mutatenm(node.branchset[1]).map(getName)
+		debug('has branchset')
+		let speciesListA = mutatenm(node.branchset[0])
+		let speciesListB = mutatenm(node.branchset[1])
 
-		debug('speciesA:', speciesA, 'speciesB', speciesB)
-		speciesA.forEach(species1 => {
-			debug(`included: ${speciesB.includes(species1)}; all equal: ${speciesB.every(equals(species1))}`)
-			if (speciesB.includes(species1) && !speciesB.every(equals(species1))) {
-				speciesB.forEach(species2 => {
-					if (species2 !== species1) {
+		debug('speciesListA:', speciesListA, 'speciesListB', speciesListB)
+		speciesListA.forEach(species1 => {
+			let hasName = pluck(speciesListB, 'name').includes(species1.name)
+			let allEqual = !(pluck(speciesListB, 'name').every(n => n === species1))
+			debug(`included: ${hasName}; all equal: ${allEqual}`)
+
+			if (hasName && allEqual) {
+				speciesListB.forEach(species2 => {
+					if (species2.name !== species1.name) {
 						marknm(node, species1, species2)
 						recordnm(species1, species2)
-						debug("marknm called on ", species1, " and ", species2)
+						debug(`marknm called on ${species1} and ${species2}`)
 					}
 				})
 
-				speciesA.forEach(species3 => {
-					if (species3 !== species1) {
+				speciesListA.forEach(species3 => {
+					if (species3.name !== species1.name) {
 						marknm(node, species1, species3)
 						recordnm(species1, species3)
-						debug("marknm called on ", species1, " and ", species3)
+						debug(`marknm called on ${species1} and ${species3}`)
 					}
 				})
 			}
 		})
-		let speciesList = [].concat(speciesA, speciesB)
+		let speciesList = [].concat(speciesListA, speciesListB)
 		debug('speciesList', speciesList)
 
 		return speciesList
 	}
 
 	debug(`no branchset, name: ${node.name}, ident: ${node.ident}`)
-	return [node.name]
+	return [node]
 }
+
 
 
 function main() {
