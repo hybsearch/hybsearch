d6bc55f9e04796995bbe537f13171f01d745d293
Hawken Rives <hawkrives@gmail.com>
2016-05-05 20:08:59 -0500

    add nexus-to-fasta converter script

---
+++ bin/nexus-to-fasta.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+'use strict'
+
+const child = require('child_process')
+const tempfile = require('tempfile')
+const fs = require('fs')
+const path = require('path')
+const getData = require('./lib_get-data')
+
+module.exports = seqmagick
+function seqmagick(data) {
+	const inputFile = tempfile().replace(' ', '\ ')
+	const outputFile = tempfile().replace(' ', '\ ')
+	fs.writeFileSync(inputFile, data, 'utf-8')
+
+	let executable = `python ${path.join('vendor', 'seqmagick', 'cli.py')}`
+	child.execSync(`${executable} convert --input-format nexus --output-format fasta --alphabet dna ${inputFile} ${outputFile}`)
+
+	// seqmagick wraps the identifiers in quotes.
+	// mrbayes does not like single quotes.
+	// remove them.
+	let output = fs.readFileSync(outputFile, 'utf-8')
+	output = output.replace(/'/g, "")
+
+	return output
+}
+
+function main() {
+	let file = process.argv[2]
+
+	if (!file && process.stdin.isTTY) {
+		console.error('usage: node nexus-to-fasta.js (<input> | -)')
+		process.exit(1)
+	}
+
+	getData(file)
+		.then(seqmagick)
+		.then(data => console.log(data))
+		.catch(console.error.bind(console))
+}
+
+if (require.main === module) {
+	main()
+}
