c1b24f1df37550fe79234fbdca84164a1e6a4bfb
Hawken Rives <hawkrives@gmail.com>
2016-05-05 20:09:25 -0500

    update the compare-ent script
    it needs to be renamed too

--- compare-ent.sh
+++ compare-ent.sh
@@ -1,25 +1,34 @@
-#!/bin/sh
+#!/bin/bash
+TIME=/usr/bin/time
 
-for file in data/*.gb; do
+function pipeline() {
+	file=$1
 	echo "$file"
 	BASE=$(basename "$file" ".gb")
-	TIME=/usr/bin/time
-	# CLUSTAL=/Users/hawken/Downloads/clustal-omega-1.2.1/compiled/bin/clustalo
 
-	{
-		echo "genbank-to-fasta"
-		$TIME bin/genbank-fasta.js    "$file"                     > "data/$BASE.fasta"
-		echo "clustal"
-		$TIME bin/clustal-o.js        "data/$BASE.fasta"          > "data/$BASE.aln.fasta"
-		# $TIME $CLUSTAL --in "data/$BASE.fasta" --out "data/$BASE.aln.fasta" --force --threads=8
-		echo "fasta-to-nexus"
-		$TIME bin/fasta-to-nexus.js   "data/$BASE.aln.fasta"      > "data/$BASE.aln.nexus"
-		echo "mrbayes"
-		$TIME bin/mrbayes.js          "data/$BASE.aln.nexus"      > "data/$BASE.tree.nexus"
-		echo "consensus-tree"
-		$TIME bin/consensus-newick.js "data/$BASE.tree.nexus"     > "data/$BASE.tree"
-		echo "tree-to-json"
-		$TIME bin/newick-json.js      "data/$BASE.tree" > "data/$BASE.tree.json"
-	}
+	echo "genbank-to-fasta"
+	$TIME bin/genbank-fasta.js    "$file"                     > "data/$BASE.fasta"
+	echo "clustal"
+	$TIME bin/clustal-o.js        "data/$BASE.fasta"          > "data/$BASE.aln.fasta"
+	# $TIME $CLUSTAL --in "data/$BASE.fasta" --out "data/$BASE.aln.fasta" --force --threads=8
+	echo "fasta-to-nexus"
+	$TIME bin/fasta-to-nexus.js   "data/$BASE.aln.fasta"      > "data/$BASE.aln.nexus"
+	echo "mrbayes"
+	$TIME bin/mrbayes.js          "data/$BASE.aln.nexus"      > "data/$BASE.tree.nexus"
+	echo "consensus-tree"
+	$TIME bin/consensus-newick.js "data/$BASE.tree.nexus"     > "data/$BASE.tree"
+	echo "tree-to-json"
+	$TIME bin/newick-json.js      "data/$BASE.tree" > "data/$BASE.tree.json"
+
 	echo
+}
+
+if test $# -ge 1; then
+	files=$*
+else
+	files=$(ls data/*.gb)
+fi
+
+for f in $files; do
+	pipeline "$f"
 done
