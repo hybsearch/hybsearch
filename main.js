b65ad0bba55d115f4bb3f0c987d1202c439dd9a7
Hawken Rives <hawkrives@gmail.com>
2016-05-11 11:59:10 -0500

    update electron

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 'use strict'
 
-const app = require('app')  // Module to control application life.
-const BrowserWindow = require('browser-window')  // Module to create native browser window.
+const electron = require('electron')
+const app = electron.app  // Module to control application life.
+const BrowserWindow = electron.BrowserWindow  // Module to create native browser window.
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
