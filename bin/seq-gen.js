972f41f6c022f03343c50ca99ac6690718e65112
Hawken Rives <hawkrives@gmail.com>
2016-06-19 18:31:22 -0500

    make seq-gen more flexible via cmd-line args

--- bin/seq-gen.js
+++ bin/seq-gen.js
@@ -8,12 +8,12 @@ const getData = require('../lib/get-data')
 const minimist = require('minimist')
 
 module.exports = seqgen
-function seqgen(data, seqLen=300, mutationRate='0.02') {
+function seqgen(data, seqLen=300, mutationRate=0.02, generations=2) {
 	const inputFile = tempfile()
 	data = `[${seqLen}, ${mutationRate}]${data}`
 	fs.writeFileSync(inputFile, data, 'utf-8')
 
-	let args = ['-mHKY', '-n2', '-on', inputFile]
+	let args = ['-mHKY', `-n${generations}`, '-on', inputFile]
 	let output = execa.sync('seq-gen', args)
 
 	return output.stdout
@@ -21,22 +21,29 @@ function seqgen(data, seqLen=300, mutationRate='0.02') {
 
 function main() {
 	let argv = minimist(process.argv.slice(2))
+
+	// TODO: I don't understand what the input files is supposed to be.
 	let file = argv['_'][0]
+	let seqLen = argv['sequence-length']
+	let mutationRate = argv['mutation-rate']
+	let generations = argv['generations']
 
 	if (!file && process.stdin.isTTY) {
-		console.error('usage: node seq-gen.js (<input> | -) [output]')
+		console.error([
+			'usage:',
+			'node seq-gen.js (<inputFile> | -) [arguments]',
+			'',
+			'optional arguments:',
+			'  --sequence-length 300',
+			'  --mutation-rate 0.02',
+			'  --generations 2',
+		].join('\n'))
 		process.exit(1)
 	}
 
 	getData(file)
-		.then(seqgen)
-		.then(output => {
-			if (argv['_'][1] === 2) {
-				fs.writeFileSync(argv['_'][1], output, 'utf-8')
-			} else {
-				console.log(output)
-			}
-		})
+		.then(data => seqgen(data, seqLen, mutationRate, generations))
+		.then(console.log.bind(console))
 		.catch(console.error.bind(console))
 }
 
