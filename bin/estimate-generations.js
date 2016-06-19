5f02f22d4caf2a391eb42b0bf068ac3720ca6a87
Hawken Rives <hawkrives@gmail.com>
2016-06-19 18:29:36 -0500

    extracted the estimateGenerations code into lib/

--- bin/estimate-generations.js
+++ bin/estimate-generations.js
@@ -1,38 +1,26 @@
 #!/usr/bin/env node
 'use strict'
 
-const execa = require('execa')
-
-function estimateGenerationsR(genLength, percentage) {
-	let args = ['lib/hamdis.r', 'estimate', genLength, percentage]
-	let output = execa.sync('Rscript', args)
-	return output.stdout
-}
-
-module.exports = estimateGenerations
-function estimateGenerations(genLength, percentage) {
-	let divtime = percentage / 0.02
-
-	// number of generations (used for seq gen parameters)
-	let gen = divtime * 1000000 / genLength
-	return [divtime, gen]
-}
+// [something something] estimate the number of generations for a species from
+// some pieces of information about the species
+const estimateGenerations = require('../lib/estimate-generations')
 
 function main() {
-	let method = process.argv[2]
-	let genLength = process.argv[3]
-	let percentage = process.argv[4]
+	let genLength = process.argv[2]
+	let percentage = process.argv[3]
 
-	if (genLength === undefined || percentage === undefined) {
-		console.error('usage: node estimate-generations.js <genLength> <percentage>')
+	if (genLength === undefined) {
+		console.error('usage: node estimate-generations.js <genLength> [percentage]')
 		process.exit(1)
 	}
 
-	let [divtime, gen] = estimateGenerations(genLength, percentage)
-	console.log("divtime=%d", divtime)
-	console.log("gen=%d", gen)
-	// let output = estimateGenerationsR(genLength, percentage)
-	// console.log(output)
+	let {
+		divergenceTime,
+		generationCount,
+	} = estimateGenerations(genLength, percentage)
+
+	console.log("divtime=%d", divergenceTime)
+	console.log("gen=%d", generationCount)
 }
 
 if (require.main === module) {
