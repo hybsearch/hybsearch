4543958398333d16cb9df241faaf67558dfab588
Hawken Rives <hawkrives@gmail.com>
2018-02-20 23:18:55 -0600

    allow passing data to worker-runner via stdin

--- scripts/worker-runner.js
+++ scripts/worker-runner.js
@@ -1,6 +1,7 @@
+#!/usr/bin/env node
 'use strict'
 
-const fs = require('fs')
+const getData = require('../lib/get-data')
 const path = require('path')
 const childProcess = require('child_process')
 
@@ -9,34 +10,41 @@ if (process.argv.length < 3) {
 	process.exit(1)
 }
 
-const filepath = process.argv[2]
-console.log(`processing ${filepath}`)
-const data = fs.readFileSync(filepath, 'utf-8')
+async function main() {
+	const filepath = process.argv[2]
+	console.log(`processing ${filepath}`)
+	const data = await getData(filepath)
 
-const child = childProcess.fork(path.join(__dirname, '..', 'lib', 'worker.js'))
+	const child = childProcess.fork(path.join(__dirname, '..', 'lib', 'worker.js'))
 
-child.on('message', communique => {
-	let [cmd, msg] = communique
+	child.on('message', communique => {
+		let [cmd, msg] = communique
 
-	switch (cmd) {
-		case 'error':
-			console.error('error!', msg)
-		case 'exit':
-			console.log()
-			console.log('exit')
-		case 'finish':
-			console.log('finish')
-			console.log()
-			console.log('the newick tree is below:')
-			console.log()
-			console.log(msg)
-		default:
-			console.log(cmd, msg)
-	}
+		switch (cmd) {
+			case 'error':
+				console.error('error!', msg)
+				break
+			case 'exit':
+				console.log()
+				console.log('exit')
+				break
+			case 'finish':
+				console.log('finish')
+				console.log()
+				console.log('the newick tree is below:')
+				console.log()
+				console.log(msg)
+				break
+			default:
+				console.log(cmd, msg)
+		}
 
-	if (cmd === 'exit' || cmd === 'error') {
-		child.disconnect()
-	}
-})
+		if (cmd === 'exit' || cmd === 'error') {
+			child.disconnect()
+		}
+	})
 
-child.send(['start', filepath, data])
+	child.send(['start', filepath, data])
+}
+
+main()
