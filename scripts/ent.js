a29cdc5636b2f581c6240adb22e0c4497651c31a
Hawken Rives <hawkrives@gmail.com>
2018-03-12 23:25:09 -0500

    extract cli from ent.js

---
+++ scripts/ent.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+'use strict'
+
+const getData = require('./get-data')
+const {strictSearch, formatData} = require('../server/lib/ent')
+
+function main() {
+	let file = process.argv[2]
+
+	if (!file && process.stdin.isTTY) {
+		console.error('usage: node ent.js (<input> | -)')
+		process.exit(1)
+	}
+
+	return getData(file)
+		.then(d => JSON.parse(d))
+		.then(strictSearch)
+		.then(formatData)
+		.then(console.log.bind(console))
+		.catch(console.error.bind(console))
+}
+
+if (require.main === module) {
+	main()
+}
