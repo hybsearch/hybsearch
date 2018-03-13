0e5c38b05a1cf01c5be2d166e868f113f2116f28
Hawken Rives <hawkrives@gmail.com>
2018-03-13 00:14:10 -0500

    rewrite ent test as jest snapshot test

--- tests/ent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-for file in tests/ent/input/*; do
-    filename="$(basename "$file")"
-    echo "testing $filename"
-    expectation="tests/ent/expected/$filename"
-    scripts/convert-newick-tree-to-json.js < "$file" | scripts/ent.js - | diff "$expectation" -
-done
