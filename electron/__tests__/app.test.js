2fbed4031f7a53c575677fa6329fc02dbdf841c5
Hawken Rives <rives@stolaf.edu>
2018-03-21 17:44:12 -0500

    reindent app.test.js

--- electron/__tests__/app.test.js
+++ electron/__tests__/app.test.js
@@ -6,18 +6,21 @@ let path = require('path')
 
 let app
 beforeEach(async () => {
-  app = new Application({path: electron, args: [path.join(__dirname, '..', 'main.js')] })
-  await app.start()
+	app = new Application({
+		path: electron,
+		args: [path.join(__dirname, '..', 'main.js')],
+	})
+	await app.start()
 })
 
 afterEach(async () => {
-  await app.isRunning() && await app.stop()
+	;(await app.isRunning()) && (await app.stop())
 })
 
 test('verify a visible window is opened with a title', async () => {
-  // Check if the window is visible
-  expect(await app.browserWindow.isVisible()).toBe(true)
+	// Check if the window is visible
+	expect(await app.browserWindow.isVisible()).toBe(true)
 
-  // Verify the window's title
-  expect(await app.client.getTitle()).toBe('Hybsearch')
+	// Verify the window's title
+	expect(await app.client.getTitle()).toBe('Hybsearch')
 })
