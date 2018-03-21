ae5c2d530f5a507ca2249102cbb2bc00eac04353
Hawken Rives <rives@stolaf.edu>
2018-03-21 14:10:43 -0500

    rename variables in nexus-to-fasta for camelCase rule

--- server/formats/nexus-to-fasta.js
+++ server/formats/nexus-to-fasta.js
@@ -4,25 +4,25 @@ const startsWith = require('lodash/startsWith')
 
 module.exports = convert
 function convert(data) {
-	let all_sets = []
+	let allSets = []
 	let dataset = null
-	let in_matrix = false
+	let inMatrix = false
 
 	for (let line of data.split('\n')) {
 		if (startsWith(line, '\tMatrix')) {
 			dataset = []
-			in_matrix = true
+			inMatrix = true
 		} else if (startsWith(line, '\t;')) {
-			in_matrix = false
-			all_sets.push(dataset)
+			inMatrix = false
+			allSets.push(dataset)
 			dataset = null
-		} else if (in_matrix) {
+		} else if (inMatrix) {
 			dataset.push(line)
 		}
 	}
 
 	let fasta = []
-	for (let dataset of all_sets) {
+	for (let dataset of allSets) {
 		for (let seq of dataset) {
 			let [ident, sequence] = seq.split(/\s+/)
 			fasta.push('>' + ident)
