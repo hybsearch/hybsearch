328288f8d65c9b31e826a44bbb936e8558c1f8f6
Hawken Rives <rives@stolaf.edu>
2018-03-21 12:06:11 -0500

    update ent tests to provide fasta data too

--- server/ent/__tests__/ent.test.js
+++ server/ent/__tests__/ent.test.js
@@ -1,17 +1,21 @@
 /* eslint-env jest */
-const { strictSearch, formatData } = require('../index')
+const { strictSearch: search, formatData } = require('../index')
 const { parse: newickToJson } = require('../../newick')
 const fs = require('fs')
 const path = require('path')
 
 const base = path.join(__dirname, 'input')
-const files = fs.readdirSync(base).filter(f => f.endsWith('.tree'))
+const files = fs
+	.readdirSync(base)
+	.filter(f => f.endsWith('.tree'))
+	.map(f => f.replace(/(.*)\.tree$/, '$1'))
 
 for (const file of files) {
 	test(file, () => {
-		const content = fs.readFileSync(path.join(base, file), 'utf-8')
+		const content = fs.readFileSync(path.join(base, file) + '.tree', 'utf-8')
+		const fasta = fs.readFileSync(path.join(base, file) + '.fasta', 'utf-8')
 		const tree = newickToJson(content)
-		const actual = formatData(strictSearch(tree))
+		const actual = formatData(search(tree, fasta))
 		expect(actual).toMatchSnapshot()
 	})
 }
