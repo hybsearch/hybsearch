f8182b21146e8bddf26f095c1a1ed573635cdc8d
Hawken Rives <hawkrives@gmail.com>
2018-04-03 22:21:47 -0500

    extract pipeline list to separate files

---
+++ server/pipeline/pipelines/search.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const ent = require('../../ent')
+const { consensusTreeToNewick, parse: parseNewick } = require('../../newick')
+const { genbankToFasta, fastaToNexus } = require('../../formats')
+const { pruneOutliers } = require('../../lib/prune-newick')
+const clustal = require('../../wrappers/clustal')
+const mrBayes = require('../../wrappers/mrbayes')
+const { removeCircularLinks } = require('../lib')
+
+module.exports = [
+	{
+		// the first step: ensures that the input is converted to FASTA
+		input: ['source'],
+		transform: ([{ filepath, contents }]) =>
+			filepath.endsWith('.fasta')
+				? [contents]
+				: [genbankToFasta(contents)],
+		output: ['initial-fasta'],
+	},
+	{
+		// aligns the FASTA sequences
+		input: ['initial-fasta'],
+		transform: ([data]) => [clustal(data)],
+		output: ['aligned-fasta'],
+	},
+	{
+		// converts the aligned FASTA into Nexus
+		input: ['aligned-fasta'],
+		transform: ([data]) => [fastaToNexus(data)],
+		output: ['aligned-nexus'],
+	},
+	{
+		// does whatever mrbayes does
+		input: ['aligned-nexus'],
+		transform: ([data]) => [mrBayes(data)],
+		output: ['consensus-tree'],
+	},
+	{
+		// turns MrBayes' consensus tree into a Newick tree
+		input: ['consensus-tree'],
+		transform: ([data]) => [consensusTreeToNewick(data)],
+		output: ['newick-tree'],
+	},
+	{
+		// turns the Newick tree into a JSON object
+		input: ['newick-tree'],
+		transform: ([data]) => [parseNewick(data)],
+		output: ['newick-json:1'],
+	},
+	{
+		input: ['newick-json:1', 'aligned-fasta'],
+		transform: ([newickJson, alignedFasta]) => {
+			let { removedData, prunedNewick } = pruneOutliers(
+				newickJson,
+				alignedFasta
+			)
+			return [prunedNewick, removedData]
+		},
+		output: ['newick-json:2', 'pruned-identifiers'],
+	},
+	{
+		// identifies the non-monophyletic sequences
+		input: ['newick-json:2', 'aligned-fasta'],
+		transform: ([newickJson, alignedFasta]) => [
+			removeCircularLinks(ent.strictSearch(newickJson, alignedFasta)),
+			removeCircularLinks(newickJson),
+		],
+		output: ['nonmonophyletic-sequences', 'newick-json:3'],
+	},
+]
--- server/pipeline/worker.js
+++ server/pipeline/worker.js
@@ -4,12 +4,7 @@ const serializeError = require('serialize-error')
 
 const Cache = require('./cache')
 const zip = require('lodash/zip')
-const ent = require('../ent')
-const { consensusTreeToNewick, parse: parseNewick } = require('../newick')
-const { genbankToFasta, fastaToNexus } = require('../formats')
-const { pruneOutliers } = require('../lib/prune-newick')
-const clustal = require('../wrappers/clustal')
-const mrBayes = require('../wrappers/mrbayes')
+const PIPELINES = require('./pipelines')
 
 /////
 ///// helpers
@@ -29,17 +24,6 @@ const stageComplete = ({ stage, result, timeTaken, cached }) =>
 const stageStart = ({ stage }) =>
 	send({ type: 'stage-start', payload: { stage } })
 const exit = () => send({ type: 'exit' })
-
-function removeCircularLinks(obj) {
-	return JSON.parse(
-		JSON.stringify(obj, function(key, val) {
-			if (['parent', 'nmInner', 'nmOuter'].includes(key)) {
-				return undefined
-			}
-			return val
-		})
-	)
-}
 
 const now = () => {
 	let time = process.hrtime()
@@ -68,79 +52,6 @@ process.on('disconnect', () => {
 ///// pipeline
 /////
 
-const hybridizationSearch = [
-	{
-		// the first step: ensures that the input is converted to FASTA
-		input: ['source'],
-		transform: ([{ filepath, contents }]) =>
-			filepath.endsWith('.fasta') ? [contents] : [genbankToFasta(contents)],
-		output: ['initial-fasta'],
-	},
-	{
-		// aligns the FASTA sequences
-		input: ['initial-fasta'],
-		transform: ([data]) => [clustal(data)],
-		output: ['aligned-fasta'],
-	},
-	{
-		// converts the aligned FASTA into Nexus
-		input: ['aligned-fasta'],
-		transform: ([data]) => [fastaToNexus(data)],
-		output: ['aligned-nexus'],
-	},
-	{
-		// does whatever mrbayes does
-		input: ['aligned-nexus'],
-		transform: ([data]) => [mrBayes(data)],
-		output: ['consensus-tree'],
-	},
-	{
-		// turns MrBayes' consensus tree into a Newick tree
-		input: ['consensus-tree'],
-		transform: ([data]) => [consensusTreeToNewick(data)],
-		output: ['newick-tree'],
-	},
-	{
-		// turns the Newick tree into a JSON object
-		input: ['newick-tree'],
-		transform: ([data]) => [parseNewick(data)],
-		output: ['newick-json:1'],
-	},
-	{
-		input: ['newick-json:1', 'aligned-fasta'],
-		transform: ([newickJson, alignedFasta]) => {
-			let { removedData, prunedNewick } = pruneOutliers(
-				newickJson,
-				alignedFasta
-			)
-			return [prunedNewick, removedData]
-		},
-		output: ['newick-json:2', 'pruned-identifiers'],
-	},
-	{
-		// identifies the non-monophyletic sequences
-		input: ['newick-json:2', 'aligned-fasta'],
-		transform: ([newickJson, alignedFasta]) => [
-			removeCircularLinks(ent.strictSearch(newickJson, alignedFasta)),
-			removeCircularLinks(newickJson),
-		],
-		output: ['nonmonophyletic-sequences', 'newick-json:3'],
-	},
-]
-
-const parseNewickFile = [
-	{
-		// turns the Newick tree into a JSON object
-		input: ['source'],
-		transform: ([data]) => [parseNewick(data)],
-		output: ['newick-json:1'],
-	},
-]
-
-const PIPELINES = {
-	search: hybridizationSearch,
-	'parse-newick': parseNewickFile,
-}
 
 // eslint-disable-next-line no-unused-vars
 async function main({ pipeline: pipelineName, filepath, data }) {
