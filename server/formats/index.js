12947f559099d610e0aa509c8294531fa7d70791
Hawken Rives <rives@stolaf.edu>
2018-04-05 13:24:35 -0500

    export fastaToBeast from /server/formats

--- server/formats/index.js
+++ server/formats/index.js
@@ -1,12 +1,14 @@
 'use strict'
 
 const fastaToNexus = require('./fasta-to-nexus')
+const fastaToBeast = require('./fasta-to-beast')
 const genbankToFasta = require('./genbank-to-fasta')
 const nexusToFasta = require('./nexus-to-fasta')
 const fasta = require('./fasta')
 
 module.exports = {
 	fastaToNexus: fastaToNexus,
+	fastaToBeast: fastaToBeast,
 	genbankToFasta: genbankToFasta,
 	nexusToFasta: nexusToFasta,
 	parseFasta: fasta.parse,
