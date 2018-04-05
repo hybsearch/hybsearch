498f9c21e0c991833ef76867c7bc42dd1a3763e7
Hawken Rives <rives@stolaf.edu>
2018-04-05 15:16:25 -0500

    update consensus-to-newick for BEAST trees and add tests

--- server/newick/consensus-to-newick.js
+++ server/newick/consensus-to-newick.js
@@ -5,16 +5,20 @@ const os = require('os')
 module.exports = consensusTreeNewick
 function consensusTreeNewick(input) {
 	input = input.replace(RegExp(os.EOL, 'g'), '\n')
-	let labelRegex = /taxlabels(\n\s*[^;]*)+\s;/
+
+	let labelRegex = /taxlabels(\n\s*[^;]*)+\s;/i
 	let labels = labelRegex.exec(input)[1]
 	labels = labels
 		.split('\n')
 		.map(l => l.trim())
-		.filter(x => x)
+		.filter(x => Boolean(x))
 
-	let treeRegex = /tree\s+\S+\s+=\s+\[[^\n\r\]]+\]\s+([^;\n\r]+;)/g
-	let conTree = treeRegex.exec(input)[1]
-	conTree = conTree.replace(/\[[^\]]+\]/g, '')
+	let treeRegex = /tree\s+\S+\s+=\s+(?:\[[^\n\r\]]+\]\s+)?([^;\n\r]+;)/gi
+	let trees = treeRegex.exec(input).slice(1)
+
+	let lastTree = trees[trees.length - 1]
+
+	let conTree = lastTree.replace(/\[[^\]]+\]/g, '')
 
 	for (let i = 0; i < labels.length; i++) {
 		// replace numbers with names
