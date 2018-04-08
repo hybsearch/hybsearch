55b09cb3a5684a5e7dd8863bfeaa6b3a8416af57
Hawken Rives <hawkrives@gmail.com>
2018-04-07 21:54:01 -0500

    add flow to module.exports.removeCircularLinks

--- server/server/lib.js
+++ server/server/lib.js
@@ -1,4 +1,6 @@
-module.exports.removeCircularLinks = function(obj) {
+// @flow
+
+module.exports.removeCircularLinks = function(obj: mixed) {
 	return JSON.parse(
 		JSON.stringify(obj, function(key, val) {
 			if (['parent', 'nmInner', 'nmOuter'].includes(key)) {
