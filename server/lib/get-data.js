79f5825f9ed8caf6f9d1a7658679a94c5980c822
Hawken Rives <hawkrives@gmail.com>
2018-04-07 21:57:17 -0500

    add types to server/get-data

--- server/lib/get-data.js
+++ server/lib/get-data.js
@@ -1,17 +1,19 @@
+// @flow
+
 require('loud-rejection/register')
 const stdin = require('get-stdin')
 const fs = require('fs')
 
-module.exports = function getData(fileOrStdin) {
+module.exports = function getData(fileOrStdin: string): Promise<string> {
 	if (!fileOrStdin || fileOrStdin === '-') {
 		return stdin()
 	}
 
-	return readFilePromise(fileOrStdin)
+	return readFile(fileOrStdin)
 }
 
-module.exports.readFile = readFilePromise
-function readFilePromise(filename) {
+module.exports.readFile = readFile
+function readFile(filename: string): Promise<string> {
 	return new Promise((resolve, reject) => {
 		fs.readFile(filename, 'utf-8', (err, contents) => {
 			if (err) {
