939313dbf559e5d33ecabce2d4a81ba5b8339eb0
Hawken Rives <hawkrives@gmail.com>
2018-04-08 00:51:08 -0500

    automate create of name:pipe map in Pipelines

--- server/server/pipelines/index.js
+++ server/server/pipelines/index.js
@@ -3,10 +3,14 @@
 
 import { type PipelineRecord } from './types'
 
-let pipelines: Map<string, PipelineRecord> = new Map([
-	['beast', require('./beast.js')],
-	['mrbayes', require('./search.js')],
-	['parse-newick', require('./parse-newick.js')],
-])
+let _pipelines = [
+	require('./beast.js'),
+	require('./search.js'),
+	require('./parse-newick.js'),
+]
+
+let pipelines: Map<string, PipelineRecord> = new Map(
+	_pipelines.map(p => [p.name, p])
+)
 
 module.exports = pipelines
