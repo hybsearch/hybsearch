80297e2a70240eadc3644e7e40db478336bcf553
Hawken Rives <hawkrives@gmail.com>
2018-04-07 21:57:29 -0500

    promote types in server/hash-string

--- server/lib/hash-string.js
+++ server/lib/hash-string.js
@@ -3,7 +3,7 @@
 
 const crypto = require('crypto')
 
-module.exports = (data /*: string*/) /* : string */ => {
+module.exports = (data: string): string => {
 	let hash = crypto.createHash('sha256')
 	hash.update(data)
 	return hash.digest('hex')
