13a574841bfba3b4b98b16598aaa6159ce694c83
Hawken Rives <rives@stolaf.edu>
2018-04-09 17:21:49 -0500

    add function to generate a control file

--- server/wrappers/jml/index.js
+++ server/wrappers/jml/index.js
@@ -15,6 +15,24 @@ const readFileOr = (filepath, orValue) => {
 	}
 }
 
+// species: ['bla', 'nit', 'fol', 'pal', 'pis', 'gym', 'set', 'mul']
+// seqPerSpecies: [44, 8, 6, 12, 8, 10, 2, 2]
+const CONTROL = ({species, seqPerSpecies}) => {
+	if (species.length !== seqPerSpecies.length) {
+		throw new Error('species and seqPerSpecies do not have the same length')
+	}
+
+	return `species = ${species.join(' ')}
+seqperspecies = ${seqPerSpecies.join(' ')}
+locusrate = 0.8762
+heredityscalar = 1
+seqgencommand = -mHKY -f0.2678,0.1604,0.2031,0.3687 -t1.5161 -i0 -a0.2195 -l810
+significancelevel = 0.1
+burnin = 0
+thinning = 1
+seed = -1`
+}
+
 module.exports = jml
 async function jml(data, argv = {}) {
 	const dir = tempy.directory()
