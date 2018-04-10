f34259c415336b9c3ccee4183a3bfa70ff788cb6
Hawken Rives <rives@stolaf.edu>
2018-04-10 14:20:02 -0500

    fix include path in test for hashNexusNames

--- server/formats/nexus/__tests__/hash-names.js
+++ server/formats/nexus/__tests__/hash-names.js
@@ -1,6 +1,6 @@
 /* eslint-env jest */
 
-const { hashNexusTreeNames } = require('../hash-names')
+const hashNexusTreeNames = require('../hash-names')
 
 test('emydura.nexus', () => {
 	let content = `#NEXUS
