1538eb437ee552b59b0b241ae983d1449b2ee142
Hawken Rives <rives@stolaf.edu>
2018-04-09 19:06:29 -0500

    add tests for hash-names and fix hash-names

--- server/formats/fasta/hash-names.js
+++ server/formats/fasta/hash-names.js
@@ -8,18 +8,41 @@ let makeHash = str => {
 	return hash.digest('base64')
 }
 
-const {parseFastaIteratively} = require('./parse')
+const {parseFasta} = require('./parse')
 
 module.exports = hashFastaSequenceNames
 function hashFastaSequenceNames(fastaData) {
 	let output = Object.create(null)
 	let speciesCounter = new Map()
+	let fullHashes = new Map()
 
-	for (let {species} of parseFastaIteratively(fastaData)) {
+	let samples = parseFasta(fastaData)
+
+	for (let {species} of samples) {
 		let [speciesName] = species.split('__')
-		let hashedSpeciesName = makeHash(speciesName).substr(0, 7)
-		let currentCount = speciesCounter.set((speciesCounter.get(hashedSpeciesName) || 0) + 1)
-		output[`${hashedSpeciesName}${currentCount}`] = species
+		let hashedSpeciesName = makeHash(speciesName).substr(0, 11)
+
+		speciesCounter.set(hashedSpeciesName, (speciesCounter.get(hashedSpeciesName) || 0) + 1)
+		fullHashes.set(speciesName, hashedSpeciesName)
+	}
+
+	let speciesCounter2 = new Map()
+	for (let {species} of samples) {
+		let [speciesName] = species.split('__')
+		let hashedSpeciesName = fullHashes.get(speciesName)
+		let count = speciesCounter.get(hashedSpeciesName)
+
+		let digitLen = count.toString().length
+		let newHashedSpeciesNameLength = hashedSpeciesName.length - 1 - digitLen
+
+		let trimmedName = hashedSpeciesName.substr(0, newHashedSpeciesNameLength)
+		speciesCounter2.set(hashedSpeciesName, (speciesCounter2.get(hashedSpeciesName) || 0) + 1)
+
+		let currentCount = speciesCounter2
+			.get(hashedSpeciesName)
+			.toString()
+			.padStart(digitLen, '0')
+		output[`${trimmedName}_${currentCount}`] = species
 	}
 
 	return output
