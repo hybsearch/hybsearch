b496f3965ef7e2b46a49fe990dbf57b50cb8ac1a
Hawken Rives <rives@stolaf.edu>
2018-04-10 15:08:38 -0500

    export the remove-identifiers module from formats/index

--- server/formats/fasta/index.js
+++ server/formats/fasta/index.js
@@ -3,5 +3,11 @@
 const build = require('./build')
 const parse = require('./parse')
 const hashFastaSequenceNames = require('./hash-names')
+const removeFastaIdentifiers = require('./remove-identifiers')
 
-module.exports = { build, parse, hashFastaSequenceNames }
+module.exports = {
+	build,
+	parse,
+	hashFastaSequenceNames,
+	removeFastaIdentifiers,
+}
