20bdb1f35bdae011f27c3629623181e1f871ad29
Hawken Rives <rives@stolaf.edu>
2018-04-09 19:17:53 -0500

    fix bug in fastaaToPhylip

--- server/formats/fasta-to-phylip.js
+++ server/formats/fasta-to-phylip.js
@@ -16,7 +16,7 @@ function fastaToPhylip(fastaData, phylipIdentMap) {
 	})
 
 	let numberOfSequences = samples.length
-	let lengthOfSequences = samples[0].length
+	let lengthOfSequences = samples[0].sequence.length
 
 	let output = `${numberOfSequences} ${lengthOfSequences}
 ${phylipSamples.join('\n')}
