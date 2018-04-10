9eb89dcc7009847dbf2d91640cbb6c454e1821bb
Hawken Rives <rives@stolaf.edu>
2018-04-09 22:52:36 -0500

    make jml work

--- server/formats/nexus/index.js
+++ server/formats/nexus/index.js
@@ -1,6 +1,7 @@
 'use strict'
 const invert = require('lodash/invert')
 const escape = require('regexp.escape')
+const mapKeys = require('lodash/mapKeys')
 
 module.exports.hashNexusTreeNames = function hashNexusTreeNames(
 	beastTrees,
@@ -14,11 +15,13 @@ module.exports.hashNexusTreeNames = function hashNexusTreeNames(
 		.filter(x => Boolean(x))
 
 	let sequenceToHashedIdentMap = invert(phylipIdentMap)
+	sequenceToHashedIdentMap = mapKeys(sequenceToHashedIdentMap, (v, k) => k.split('__')[0])
 
 	for (let label of labels) {
 		let hashedLabel = sequenceToHashedIdentMap[label]
+		let speciesLabel = hashedLabel.split('x')[0]
 		// console.log(label, hashedLabel, escape(hashedLabel))
-		beastTrees = beastTrees.replace(new RegExp(escape(label), 'g'), hashedLabel)
+		beastTrees = beastTrees.replace(new RegExp(escape(label), 'g'), speciesLabel)
 	}
 
 	return beastTrees
