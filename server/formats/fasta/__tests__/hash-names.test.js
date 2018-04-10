b0e982db74605fbb18737746703966414a93b322
Hawken Rives <rives@stolaf.edu>
2018-04-09 19:06:29 -0500

    add tests for hash-names and fix hash-names

---
+++ server/formats/fasta/__tests__/hash-names.test.js
@@ -0,0 +1,55 @@
+/* eslint-env jest */
+'use strict'
+
+const expect = require('expect')
+const hashNames = require('../hash-names')
+
+test('shortens the accession identifiers', () => {
+	let input = `> Emydura_subglobosa__KC755190
+ggaacaataaattatcacctcaaaagacac
+> Emydura_victoriae__KC755189
+ggaacaatcaattattaccccacaaagac
+> Emydura_victoriae__KC755188
+ggaacaatcaattattaccccacaaggaa`
+
+	let hashedNames = hashNames(input)
+
+	expect(hashedNames).toMatchSnapshot()
+})
+
+test('pads accession numbers to the left with 0s when they grow past one digit', () => {
+	let input = `> Emydura_victoriae__KC755189
+ggaacaataaattatcacctcaaaagacac
+> Emydura_victoriae__KC755189
+ggaacaatcaattattaccccacaaagac
+> Emydura_victoriae__KC755188
+ggaacaatcaattattaccccacaaggaa
+> Emydura_victoriae__KC755188
+ggaacaatcaattattaccccacaaggaa
+> Emydura_victoriae__KC755188
+ggaacaatcaattattaccccacaaggaa
+> Emydura_victoriae__KC755188
+ggaacaatcaattattaccccacaaggaa
+> Emydura_victoriae__KC755188
+ggaacaatcaattattaccccacaaggaa
+> Emydura_victoriae__KC755188
+ggaacaatcaattattaccccacaaggaa
+> Emydura_victoriae__KC755188
+ggaacaatcaattattaccccacaaggaa
+> Emydura_victoriae__KC755188
+ggaacaatcaattattaccccacaaggaa
+> Emydura_victoriae__KC755188
+ggaacaatcaattattaccccacaaggaa
+> Emydura_victoriae__KC755188
+ggaacaatcaattattaccccacaaggaa
+> Emydura_victoriae__KC755188
+ggaacaatcaattattaccccacaaggaa
+> Emydura_victoriae__KC755188
+ggaacaatcaattattaccccacaaggaa
+> Emydura_victoriae__KC755188
+ggaacaatcaattattaccccacaaggaa`
+
+	let hashedNames = hashNames(input)
+
+	expect(hashedNames).toMatchSnapshot()
+})
