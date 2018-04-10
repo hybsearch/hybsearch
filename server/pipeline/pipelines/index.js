4caef18c88c7b77554bc3566a5e929263c44bad4
Hawken Rives <rives@stolaf.edu>
2018-04-10 15:27:56 -0500

    add "mbnb" pipeline to use MrBayes and exclude early identified NM results from the BEAST analysis

--- server/pipeline/pipelines/index.js
+++ server/pipeline/pipelines/index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+module.exports.mbnb = require('./mbnb.js')
 module.exports.beast = require('./beast.js')
 module.exports.search = require('./search.js')
 module.exports['parse-newick'] = require('./parse-newick.js')
