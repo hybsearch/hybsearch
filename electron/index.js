5e03634ab4dc6d8e9484f987f7377a086f333075
Hawken Rives <rives@stolaf.edu>
2018-04-12 14:09:17 -0500

    set the app title on launch

--- electron/index.js
+++ electron/index.js
@@ -1,6 +1,8 @@
 'use strict'
 
 // set the title to include the app version
+const { version } = require('./package.json')
+document.title = `HybSearch (v${version})`
 
 // start the code
 require('./ui')
