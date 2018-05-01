485943c4373ee6b3f58c8ffd1bdb4ff119292dc1
Hawken Rives <rives@stolaf.edu>
2018-05-01 16:37:06 -0500

    update ent to use the new __supporting__ global folder

--- server/ent/__tests__/ent.test.js
+++ server/ent/__tests__/ent.test.js
@@ -6,18 +6,19 @@ const { parse: newickToJson } = require('../../newick')
 const fs = require('fs')
 const path = require('path')
 
-const base = path.join(__dirname, 'input')
+const base = path.join(__dirname, '..', '..', '..', 'data', '__supporting__')
+const blacklist = ['bataguridae-cytb.gb']
 const files = fs
 	.readdirSync(base)
-	.filter(f => f.endsWith('.tree'))
-	.map(f => f.replace(/(.*)\.tree$/, '$1'))
+	.filter(name => !name.startsWith('.') && !name.startsWith('_'))
+	.filter(name => !blacklist.includes(name))
 
 for (const file of files) {
+	const read = name => fs.readFileSync(path.join(base, file, name), 'utf-8')
 	test(file, () => {
-		const content = fs.readFileSync(path.join(base, file) + '.tree', 'utf-8')
+		const fasta = read('aligned-fasta')
+		const content = read('newick-tree')
 		const tree = newickToJson(content)
-
-		const fasta = fs.readFileSync(path.join(base, file) + '.fasta', 'utf-8')
 
 		const actual = formatData(search(tree, fasta))
 		expect(actual.split('\n')).toMatchSpecificSnapshot(path.join('.', '__snapshots__', file + '.hybsnap'))
