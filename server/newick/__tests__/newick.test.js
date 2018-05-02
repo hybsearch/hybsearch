935b644db95fe040128495f24afbd7c35685f2d3
Hawken Rives <rives@stolaf.edu>
2018-05-01 19:11:51 -0500

    split newick snapshot file into multiple files

--- server/newick/__tests__/newick.test.js
+++ server/newick/__tests__/newick.test.js
@@ -1,16 +1,22 @@
 /* eslint-env jest */
+require('jest-specific-snapshot')
 
 const newickToJson = require('../parser')
 const fs = require('fs')
 const path = require('path')
 
-const base = path.join(__dirname, 'input')
-const files = fs.readdirSync(base).filter(f => f.endsWith('.tree'))
+const base = path.join(__dirname, '..', '..', '..', 'data', '__supporting__')
+const blacklist = []
+const files = fs
+	.readdirSync(base)
+	.filter(name => !name.startsWith('.') && !name.startsWith('_'))
+	.filter(name => name.endsWith('.gb'))
+	.filter(name => !blacklist.includes(name))
 
 for (const file of files) {
 	test(file, () => {
-		const content = fs.readFileSync(path.join(base, file), 'utf-8')
+		const content = fs.readFileSync(path.join(base, file, 'newick-tree'), 'utf-8')
 		const tree = newickToJson(content)
-		expect(tree).toMatchSnapshot()
+		expect(tree).toMatchSpecificSnapshot(path.join('.', '__snapshots__', file + '.hybsnap'))
 	})
 }
