822bc4bb0910f96a58e60f1e47f3e64ffd04afdc
Hawken Rives <rives@stolaf.edu>
2018-05-01 19:20:03 -0500

    update some tests in formats/

--- server/formats/__tests__/fasta-to-beast.test.js
+++ server/formats/__tests__/fasta-to-beast.test.js
@@ -1,16 +1,22 @@
 /* eslint-env jest */
+require('jest-specific-snapshot')
 
 const fastaToBeast = require('../fasta-to-beast')
 const fs = require('fs')
 const path = require('path')
 
-const base = path.join(__dirname, 'input')
-const files = fs.readdirSync(base).filter(f => f.endsWith('.fasta'))
+const base = path.join(__dirname, '..', '..', '..', 'data', '__supporting__')
+const whitelist = ['emydura-short.gb']
+const files = fs
+	.readdirSync(base)
+	.filter(name => !name.startsWith('.') && !name.startsWith('_'))
+	.filter(name => name.endsWith('.gb'))
+	.filter(name => whitelist.includes(name))
 
 for (const file of files) {
 	test(file, () => {
-		const content = fs.readFileSync(path.join(base, file), 'utf-8')
+		const content = fs.readFileSync(path.join(base, file, 'aligned-fasta'), 'utf-8')
 		const tree = fastaToBeast(content)
-		expect(tree).toMatchSnapshot()
+		expect(tree).toMatchSpecificSnapshot(path.join('.', '__snapshots__', 'fasta-to-beast.' + file + '.hybsnap'))
 	})
 }
--- server/formats/__tests__/fasta-to-phylip.test.js
+++ server/formats/__tests__/fasta-to-phylip.test.js
@@ -1,17 +1,23 @@
 /* eslint-env jest */
+require('jest-specific-snapshot')
 
 const fastaToPhylip = require('../fasta-to-phylip')
 const hashNames = require('../fasta/hash-names')
 const fs = require('fs')
 const path = require('path')
 
-const base = path.join(__dirname, 'input')
-const files = fs.readdirSync(base).filter(f => f.endsWith('.fasta'))
+const base = path.join(__dirname, '..', '..', '..', 'data', '__supporting__')
+const whitelist = ['emydura-short.gb']
+const files = fs
+	.readdirSync(base)
+	.filter(name => !name.startsWith('.') && !name.startsWith('_'))
+	.filter(name => name.endsWith('.gb'))
+	.filter(name => whitelist.includes(name))
 
 for (const file of files) {
 	test(file, () => {
-		const content = fs.readFileSync(path.join(base, file), 'utf-8')
+		const content = fs.readFileSync(path.join(base, file, 'aligned-fasta'), 'utf-8')
 		const tree = fastaToPhylip(content, hashNames(content))
-		expect(tree).toMatchSnapshot()
+		expect(tree).toMatchSpecificSnapshot(path.join('.', '__snapshots__', 'fasta-to-phylip.' + file + '.hybsnap'))
 	})
 }
