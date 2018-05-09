296ac2f64dcf9ecb7d9ca7c28a04571eacf73e82
Hawken Rives <rives@stolaf.edu>
2018-05-09 15:28:26 -0500

    create "keepIdentifiers" as the opposite of "removeIdentifiers"

---
+++ server/formats/fasta/__tests__/keep-identifiers.test.js
@@ -0,0 +1,25 @@
+/* eslint-env jest */
+'use strict'
+
+const expect = require('expect')
+const keepIdentifiers = require('../keep-identifiers')
+
+test('removes the given identifiers from the data', () => {
+	let input = `> Emydura_subglobosa__KC755190
+ggaacaataaattatcacctcaaaagacac
+> Emydura_victoriae__KC755189
+ggaacaatcaattattaccccacaaagac
+> Emydura_victoriae__KC755188
+ggaacaatcaattattaccccacaaggaa`
+
+	let filteredSequences = keepIdentifiers(input, {
+		nm: [
+			[
+				{ name: 'Emydura_subglobosa', ident: 'KC755190' },
+				{ name: 'Emydura_victoriae', ident: 'KC755189' },
+			],
+		],
+	})
+
+	expect(filteredSequences).toMatchSnapshot()
+})
---
+++ server/formats/fasta/keep-identifiers.js
@@ -0,0 +1,40 @@
+// @flow
+'use strict'
+
+const { parseFasta } = require('./parse')
+const { buildFasta } = require('./build')
+const flatten = require('lodash/flatten')
+
+/*::
+type EntNmPair = {
+	ident: string,
+	length: number,
+	name: string,
+}
+
+type EntNonmonophylyResults = {
+	nm: Array<[EntNmPair, EntNmPair]>,
+	species: any,
+}
+*/
+
+module.exports = keepFastaIdentifiers
+function keepFastaIdentifiers(
+	dataString /* : string */,
+	identifiers /* :EntNonmonophylyResults */
+) {
+	let samples = parseFasta(dataString)
+	let identifiersToRemove = new Set(
+		flatten(
+			identifiers.nm.map(pair =>
+				pair.map(node => `${node.name}__${node.ident}`)
+			)
+		)
+	)
+
+	let filtered = samples.filter(
+		({ species }) => identifiersToRemove.has(species)
+	)
+
+	return buildFasta(filtered)
+}
