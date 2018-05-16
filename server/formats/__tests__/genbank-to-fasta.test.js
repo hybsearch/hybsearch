61da083ef1adceeacf6011010f0024b3ee91ecf7
Hawken Rives <rives@stolaf.edu>
2018-05-16 15:55:39 -0500

    fix prettier

--- server/formats/__tests__/genbank-to-fasta.test.js
+++ server/formats/__tests__/genbank-to-fasta.test.js
@@ -9,9 +9,9 @@ const base = path.join(__dirname, 'input')
 const files = fs.readdirSync(base).filter(f => f.endsWith('.gb'))
 
 for (const file of files) {
-  test(file, () => {
-    const content = fs.readFileSync(path.join(base, file), 'utf-8')
-    const tree = genbankToFasta(content, hashNames(content))
-    expect(tree).toMatchSnapshot()
-  })
+	test(file, () => {
+		const content = fs.readFileSync(path.join(base, file), 'utf-8')
+		const tree = genbankToFasta(content, hashNames(content))
+		expect(tree).toMatchSnapshot()
+	})
 }
