34332e5977f19a9240609804083a928513af9bbf
Hawken Rives <rives@stolaf.edu>
2018-05-19 14:51:33 -0500

    remove in-memory cache from pipeline

--- server/pipeline/cache.js
+++ server/pipeline/cache.js
@@ -12,7 +12,6 @@ class Cache {
 		this.data = contents
 		this.pipelineName = pipelineName
 
-		this.memcache = new Map()
 
 		if (!process.env.DOCKER) {
 			this.cacheDir = tempy.directory()
@@ -45,14 +44,8 @@ class Cache {
 	get(key) {
 		key = this.hashKey(key)
 
-		if (this.memcache.has(key)) {
-			let value = this.memcache.get(key)
-			return JSON.parse(value)
-		}
-
 		try {
 			let value = fs.readFileSync(this.diskFilename(key), 'utf-8')
-			this.memcache.set(key, value)
 			return JSON.parse(value)
 		} catch (err) {
 			if (err.code === 'ENOENT') {
@@ -65,7 +58,6 @@ class Cache {
 	set(key, value) {
 		key = this.hashKey(key)
 		let serialized = JSON.stringify(value)
-		this.memcache.set(key, serialized)
 		fs.writeFileSync(this.diskFilename(key), serialized, 'utf-8')
 	}
 }
