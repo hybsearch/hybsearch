44b7b880825ed18d5b9a2684fad9b3972401b4df
Hawken Rives <rives@stolaf.edu>
2018-05-19 14:17:15 -0500

    add test case for // in genbank files

--- server/formats/__tests__/genbank-to-fasta.test.js
+++ server/formats/__tests__/genbank-to-fasta.test.js
@@ -28,3 +28,21 @@ ORIGIN
 
     expect(genbankToFasta(input)).toMatchSnapshot()
 })
+
+test('handles URLs with // in them', () => {
+	let input = `
+ACCESSION   AB817079
+  ORGANISM  Panthera pardus orientalis
+            Eukaryota; Metazoa; Chordata; Craniata; Vertebrata; Euteleostomi;
+            Mammalia; Eutheria; Laurasiatheria; Carnivora; Feliformia; Felidae;
+            Pantherinae; Panthera.
+  JOURNAL   Submitted (10-APR-2013) Contact:Taro Sugimoto Graduate School of
+            Environmental Science Hokkaido University, Devision of
+            Environmental Science Creation; Kitaku kita 10-jou nishi 5-chome,
+            Sapporo, Hokkaido 060-0810, Japan
+  URL       :http://www.ees.hokudai.ac.jp/
+ORIGIN
+        1 ccatccaaca tctcaacatg atggaacttt ggctccctat taggggtatg tttaatccta`
+
+    expect(genbankToFasta(input)).toMatchSnapshot()
+})
