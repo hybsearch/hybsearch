b75ff7a36c281baf90ba2b3e520eb66ab85bf7b8
Hawken Rives <rives@stolaf.edu>
2018-05-20 16:51:18 -0500

    remove a comment from hammingDistance

--- server/hamdis/hamming-distance.js
+++ server/hamdis/hamming-distance.js
@@ -5,8 +5,6 @@ function hammingDistance(seq1, seq2) {
 	// We want to compare the two individuals to find a Hamming distance
 	// between them. That is, how many base pairs differ between the two
 	// sequences. To do this, we must compare each base pair, one at a time.
-
-	// Track the number of base pairs that are different.
 	let counter = 0
 
 	for (let i = 0; i < seq1.length; i++) {
