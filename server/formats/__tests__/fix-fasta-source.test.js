17da96b9d8be007634c8844310aec7a465aebd54
Hawken Rives <rives@stolaf.edu>
2018-05-23 10:18:34 -0500

    remove outpluscoxone from formats tests

--- server/formats/__tests__/fix-fasta-source.test.js
+++ server/formats/__tests__/fix-fasta-source.test.js
@@ -2,19 +2,85 @@
 
 const { fixFastaSource } = require('../fix-fasta-source')
 const { parseFasta } = require('../fasta/parse')
-const fs = require('fs')
-const path = require('path')
 
-const base = path.join(__dirname, 'input')
-const files = fs.readdirSync(base).filter(f => f.endsWith('.fasta'))
+test('processes a fasta input file', () => {
+	let input = `>HQ012626.1 Trionyx triunguis voucher Tt24 cytochrome b (cytb) gene, partial cds; mitochondrial
+------------------------------------------------------------
+------------------------------------------------------------
+------------------------------------------------------------
+------------------------------------------------------------
+------------------------------------------------------------
+------------------------------------------------------------
+-------------------ACTAACCATAGCCACCGCATTCATAGGATACGTACTACCAT
+GAGGGCAAATATCCTTCTGAGG------GGCCACCGTAATCACAAACCTACTATCAGCCG
+TCCCCTATATCGGCACCACAATAGTACAATGGGTCTGAGGAGGCTTCTCCGTAGACAATG
+CCA---------CCCTAAC---------------ACGA---------TTCTTCACCATAC
+ACTTCCTACTTCCATTCATAATCGCAGGAATAACCATAGT--ACATCTACTATTCCTACA
+CGAAACTGGATCAAACAACCCAATAGGACTTAACTCAAACACAGACAAAATCCC-GTTCC
+ATCCCTACTTCTCATACAAAGACCTACTGGGCCTAACTGTAATACTAACCACACTACTAT
+CTATCGCCATATTTTACCCAAACCTATTGGGGGACCCAGACAACTTCACGCCAGCCAACC
+CACTATCCACCCCGCCCCACATCAAACCAGAATGATACTTTCTATTCGCATATGCCATCT
+TACGATCAATCCCAAACAAATTAGGCGGAGTCCTAGCCTTACTACTATCCATCCTAGTGC
+TATTCACCCTCCCCATAGTCCACACATCAAAACAACGAACACTTACCTTCCGACCAATCA
+CCCAAACCTTATTCTGACTATTCGTAGCCAACCTAATAGTACTGACATGAATTGGAGGTC
+AACCAGTAGAAAACCCATTCATTCTTATCGGACAAACAGCCTCCATCCTCTATTTCCTAA
+TCTTACTCGTACTAATACC-----------------------------------------
+-----------------------------
+>HQ012625.1 Trionyx triunguis voucher Tt23 cytochrome b (cytb) gene, partial cds; mitochondrial
+------------------------------------------------------------
+------------------------------------------------------------
+------------------------------------------------------------
+------------------------------------------------------------
+------------------------------------------------------------
+------------------------------------------------------------
+-------------------ACTAACCATAGCCACCGCATTCATAGGATACGTACTACCAT
+GAGGGCAAATATCCTTCTGAGG------GGCCACCGTAATCACAAACCTACTATCAGCCG
+TCCCCTATATCGGCACCACAATAGTACAATGGGTCTGAGGAGGCTTCTCCGTAGACAATG
+CCA---------CCCTAAC---------------ACGA---------TTCTTCACCATAC
+ACTTCCTACTTCCATTCATAATCGCAGGAATAACCATAGT--ACATCTACTATTCCTACA
+CGAAACTGGATCAAACAACCCAATAGGACTTAACTCAAACACAGACAAAATCCC-GTTCC
+ATCCCTACTTCTCATACAAAGACCTACTGGGCCTAACTGTAATACTAACCACACTACTAT
+CTATCGCCATATTTTACCCAAACCTATTGGGGGACCCAGACAACTTCACGCCAGCCAACC
+CACTATCCACCCCGCCCCACATCAAACCAGAATGATACTTTCTATTCGCATATGCCATCT
+TACGATCAATCCCAAACAAATTAGGCGGAGTCCTAGCCTTACTACTATCCATCCTAGTGC
+TATTCACCCTCCCCATAGTCCACACATCAAAACAACGAACACTTACCTTCCGACCAATCA
+CCCAAACCTTATTCTGACTATTCGTAGCCAACCTAATAGTACTGACATGAATTGGAGGTC
+AACCAGTAGAAAACCCATTCATTCTTATCGGACAAACAGCCTCCATCCTCTATTTC----
+------------------------------------------------------------
+-----------------------------
+`
+	let output = fixFastaSource(input)
+	expect(output).toMatchSnapshot()
+})
 
-for (const file of files) {
-	test(file, () => {
-		const content = fs.readFileSync(path.join(base, file), 'utf-8')
-		const tree = fixFastaSource(content)
-		expect(tree).toMatchSnapshot()
-	})
-}
+test('leaves a file with nice accession numbers alone', () => {
+	let input = `> Emydura_subglobosa__KC755190
+-ggaacaataaattatcacctcaaaagacacataaaaccacaggaaccccta-ctcaaccataaaaaacacaaatccact
+attaaaaatcattaacaacacctttattgatctccccaccccatctaacatctctactttatgaaactttggttcattac
+taggggcatgcctcattctacaactagccacaggaatcttcttagctatacactactcatctgatatttccatagcattc
+tcatcaatctcccacatccaacgagacgttcaatatggctgactaattcgaaatatacacgctaacggtgcttcattatt
+ctttatatgcatttacctccatattggacgaggaatctactacggttcatacctttacaagaaaacctgaaacactggag
+taatattactactcctagtcatagccactgcattcgtgggctacgtactaccatgaggacaaatatcattctgaggagct
+acagtaatcaccaatctcctatcagccattccatatgtaggccctacacttgtagaatgaatttgaggggggttctccgt
+agataacgccacccttacccgattctttacattccacttcctaatcccattcgccatcctaggaataactatactacatc
+tactactactacatgaaacaggatcaaacaacccaacaggactaaactcaaactgtgacaaaatcccattccacccatat
+ttctcctataaagatctactaggccttattctaataataacactcctactcacccttaccctattctaccctaacctact
+aggagatccagacaactttacaccagccaacccactaaccacc
+> Emydura_victoriae__KC755189
+ggaacaatcaattattaccccacaaagacacataaaaccacaggaacccctactccaaccataaaaaacacaaatccact
+attaaaaatcattaacaacaccttcgttgatcttcccaccccatccaacatctccgctttatgaaacttcggatcattac
+taggagcatgcctcattctacaactagccacaggaatcttcttagctatacactactcatccgacatttctacagcattc
+tcatcaatttcccacatccaacgagatgttcaatatggatgacttattcgaaatatacacgccaacggtgcttcattatt
+ctttatatgcatctaccttcatattggacgagggatctactacggttcctacctttacaacaaaacctgaaacactggag
+taatattactactcctagttatagccactgcattcgtgggctacgtgctaccatgaggccaaatatcattctgaggggct
+acagtaatcaccaacctattatcagccattccatacgtaggcccaacacttgtagagtggatttgaggagggttctccgt
+agacaacgctactcttactcgattcttcacatttcactttctaatcccattcgccatcctaggaataaccctactacacc
+tcctacttctacacgaaacaggatcaaacaacccaacaggactaaactcaaactgtgacaaaatcccattccacccatat
+ttctcgtataaagacctactaggcctcatcctaataattatatgtctactcacccttaccctatttcacccaaacctact
+aggagatccagacaactttacaccagccaatccactaaccacc`
+	let output = fixFastaSource(input)
+	expect(output).toBe(input)
+})
 
 test('leaves properly-formatted identifiers alone', () => {
 	let ident = 'Trionyx_triunguis__HQ012626'
