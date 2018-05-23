61a602640e39b997ee44b0c760f5fb31f08247c8
Hawken Rives <rives@stolaf.edu>
2018-05-23 09:41:22 -0500

    add a module to clean up fasta source files
    this is only activated if we get a fasta file, not a genbank one

---
+++ server/formats/__tests__/fix-fasta-source.test.js
@@ -0,0 +1,47 @@
+/* eslint-env jest */
+
+const { fixFastaSource } = require('../fix-fasta-source')
+const fs = require('fs')
+const path = require('path')
+
+const base = path.join(__dirname, 'input')
+const files = fs.readdirSync(base).filter(f => f.endsWith('.fasta'))
+
+for (const file of files) {
+	test(file, () => {
+		const content = fs.readFileSync(path.join(base, file), 'utf-8')
+		const tree = fixFastaSource(content)
+		expect(tree).toMatchSnapshot()
+	})
+}
+
+test('leaves properly-formatted identifiers alone', () => {
+	let ident = 'Trionyx_triunguis__HQ012626'
+	let input = `>${ident}
+-------------------ACTAACCATAGCCACCGCATTCATAGGATACGTACTACCAT`
+	expect(fixFastaSource(input)[0].species).toBe(ident)
+})
+
+test('formats an accession+species identifier (where the accession has a period)', () => {
+	let input = `>HQ012626.1 Trionyx triunguis voucher Tt24 cytochrome b (cytb) gene, partial cds; mitochondrial
+-------------------ACTAACCATAGCCACCGCATTCATAGGATACGTACTACCAT`
+	expect(fixFastaSource(input)[0].species).toBe('Trionyx_triunguis__HQ012626x1')
+})
+
+test('formats an accession+species identifier', () => {
+	let input = `>HQ012626 Trionyx triunguis voucher Tt24 cytochrome b (cytb) gene, partial cds; mitochondrial
+-------------------ACTAACCATAGCCACCGCATTCATAGGATACGTACTACCAT`
+	expect(fixFastaSource(input)[0].species).toBe('Trionyx_triunguis__HQ012626')
+})
+
+test('formats an accession-only identifier (with a period)', () => {
+	let input = `>HQ012626.1
+-------------------ACTAACCATAGCCACCGCATTCATAGGATACGTACTACCAT`
+	expect(fixFastaSource(input)[0].species).toBe('HQ012626x1')
+})
+
+test('formats an accession-only identifier', () => {
+	let input = `>HQ012626
+-------------------ACTAACCATAGCCACCGCATTCATAGGATACGTACTACCAT`
+	expect(fixFastaSource(input)[0].species).toBe('HQ012626')
+})
